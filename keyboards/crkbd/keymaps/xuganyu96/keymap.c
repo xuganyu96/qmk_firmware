cabf2ecf9759b9ba9a01e11e723049101f235a13
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-09-07 00:46:13 -0400

    Customize layer name in OLED

--- keyboards/crkbd/keymaps/xuganyu96/keymap.c
+++ keyboards/crkbd/keymaps/xuganyu96/keymap.c
@@ -85,3 +85,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("Symbols\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("Mouse\n"), false);
+            break;
+        case 3:
+            oled_write_P(PSTR("Board\n"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
+}
+#endif
