9ce987d6582591f20c770a4ce2e79b0e1e30f019
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-10-08 15:59:20 -0400

    Formatting

--- keyboards/ferris/keymaps/xuganyu96/keymap.c
+++ keyboards/ferris/keymaps/xuganyu96/keymap.c
@@ -3,12 +3,46 @@
 #    include "keymap.h"
 #endif
 
+#define CTL_S LCTL_T(KC_S)
+#define ALT_D LALT_T(KC_D)
+#define CMD_F LGUI_T(KC_F)
+#define SUPER_G ALL_T(KC_G)
+#define CMD_J RGUI_T(KC_J)
+#define ALT_K RALT_T(KC_K)
+#define CTL_L RCTL_T(KC_L)
+#define OSMLSFT OSM(MOD_LSFT)
+#define OSMRSFT OSM(MOD_RSFT)
+#define LT1_ENT LT(1, KC_ENT)
+#define CMD_4 RGUI_T(KC_4)
+#define ALT_5 RALT_T(KC_5)
+#define CTL_6 RCTL_T(KC_6)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x5_2(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_A, LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), ALL_T(KC_G), KC_H, RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), KC_SCLN, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_SPC, KC_ESC, OSM(MOD_LSFT), LT(1,KC_ENT)),
-    [1] = LAYOUT_split_3x5_2(KC_NO, KC_GRV, KC_LBRC, KC_RBRC, KC_NO, KC_EQL, KC_7, KC_8, KC_9, KC_BSLS, KC_TAB, KC_BSPC, KC_LPRN, KC_RPRN, KC_NO, KC_MINS, RGUI_T(KC_4), RALT_T(KC_5), RCTL_T(KC_6), KC_QUOT, OSM(MOD_LSFT), KC_NO, KC_LT, KC_GT, KC_DOT, KC_0, KC_1, KC_2, KC_3, OSM(MOD_RSFT), KC_TRNS, TO(2), KC_TRNS, KC_TRNS),
-    [2] = LAYOUT_split_3x5_2(KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_MUTE, KC_BSPC, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TAB, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_RSFT, TO(0), TO(3), KC_NO, KC_NO),
-    [3] = LAYOUT_split_3x5_2(QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_NO, KC_NO)
+    [0] = LAYOUT_split_3x5_2(
+        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
+        KC_A,    CTL_S,    ALT_D,    CMD_F,  SUPER_G,               KC_H,    CMD_J,    ALT_K,    CTL_L,  KC_SCLN,
+        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,
+                                    KC_SPC,   KC_ESC,            OSMLSFT,  LT1_ENT
+    ),
+    [1] = LAYOUT_split_3x5_2(
+        KC_NO,   KC_GRV, KC_LBRC,  KC_RBRC,  XXXXXXX,             KC_EQL,     KC_7,     KC_8,     KC_9,  KC_BSLS,
+       KC_TAB,  KC_BSPC, KC_LPRN,  KC_RPRN,  XXXXXXX,            KC_MINS,    CMD_4,    ALT_5,    CTL_6,  KC_QUOT,
+      OSMLSFT,    KC_NO,   KC_LT,    KC_GT,   KC_DOT,               KC_0,     KC_1,     KC_2,     KC_3,  OSMRSFT,
+                                   KC_TRNS,    TO(2),            KC_TRNS,  KC_TRNS
+    ),
+    [2] = LAYOUT_split_3x5_2(
+      KC_WH_U,  KC_BTN1, KC_MS_U,  KC_BTN2,    KC_NO,              KC_NO,  KC_VOLD, KC_VOLU,   KC_MUTE,  KC_BSPC,
+      KC_WH_D,  KC_MS_L, KC_MS_D,  KC_MS_R,    KC_NO,            KC_LEFT,  KC_DOWN,   KC_UP,   KC_RGHT,   KC_TAB,
+      KC_LSFT,  KC_LCTL, KC_LALT,  KC_LGUI,    KC_NO,              KC_NO,  KC_MPRV, KC_MPLY,   KC_MNXT,  KC_RSFT,
+                                     TO(0),    TO(3),              KC_NO,    KC_NO
+    ),
+    [3] = LAYOUT_split_3x5_2(
+      QK_BOOT,    KC_NO,   KC_NO,    KC_NO,    KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                                     TO(0),    KC_NO,              KC_NO,    KC_NO
+    )
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
