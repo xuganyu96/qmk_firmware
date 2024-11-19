fc13814c308674f8ea8074c91364e19d3e75b4d1
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-11-19 09:45:02 -0500

    moved space to right thumb; moved enter to layer 1

--- keyboards/crkbd/keymaps/xuganyu96/keymap.c
+++ keyboards/crkbd/keymaps/xuganyu96/keymap.c
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VOL_DOWN KC_KB_VOLUME_DOWN
 #define MUTE KC_KB_MUTE
 #define LT1ENT LT(1, KC_ENT)
+#define LT1SPC LT(1, KC_SPC)
 #define LT2TAB LT(2, KC_TAB)
 #define LT3DOT LT(3, KC_DOT)
 #define OSMRSFT OSM(MOD_RSFT)
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  LT3DOT, KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX,  KC_SPC,  LT2TAB,    OSMLSFT,  LT1ENT, XXXXXXX
+                                          XXXXXXX,  KC_SPC,  LT2TAB,    OSMLSFT,  LT1SPC, XXXXXXX
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -58,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, OSMLSFT, KC_BSPC, KC_LPRN, KC_RPRN, XXXXXXX,                     KC_MINUS,   CMD_4,   OPT_5,   CTL_6,RSFTQUOT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, KC_LABK, KC_RABK,  KC_DOT,                         KC_0,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_LABK, KC_RABK,  KC_DOT,                         KC_0,    KC_1,    KC_2,    KC_3,  KC_ENT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX, _______, _______,     _______, _______, XXXXXXX
                                       //`--------------------------'  `--------------------------'
