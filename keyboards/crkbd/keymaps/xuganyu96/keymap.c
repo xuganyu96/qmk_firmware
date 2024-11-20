4f4b18accd7d7a49942c3483a96f36004d0e0723
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-11-19 23:17:41 -0500

    swap enter and space

--- keyboards/crkbd/keymaps/xuganyu96/keymap.c
+++ keyboards/crkbd/keymaps/xuganyu96/keymap.c
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VOL_DOWN KC_KB_VOLUME_DOWN
 #define MUTE KC_KB_MUTE
 #define LT1ENT LT(1, KC_ENT)
+#define LT1SPC LT(1, KC_SPC)
 #define LT2TAB LT(2, KC_TAB)
 #define LT3DOT LT(3, KC_DOT)
 #define OSMRSFT OSM(MOD_RSFT)
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  LT3DOT, KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX,  KC_SPC,  LT2TAB,    OSMLSFT,  LT1ENT, XXXXXXX
+                                          XXXXXXX,  LT1ENT,  LT2TAB,    OSMLSFT,  LT1SPC, XXXXXXX
                                       //`--------------------------'  `--------------------------'
 
   ),
--- keyboards/ferris/keymaps/xuganyu96/keymap.c
+++ keyboards/ferris/keymaps/xuganyu96/keymap.c
@@ -12,7 +12,8 @@
 #define CTL_L RCTL_T(KC_L)
 #define OSMLSFT OSM(MOD_LSFT)
 #define OSMRSFT OSM(MOD_RSFT)
-#define LT1_ENT LT(1, KC_ENT)
+#define LT1ENT LT(1, KC_ENT)
+#define LT1SPC LT(1, KC_SPC)
 #define CMD_4 RGUI_T(KC_4)
 #define ALT_5 RALT_T(KC_5)
 #define CTL_6 RCTL_T(KC_6)
@@ -29,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
         KC_A,    CTL_S,    ALT_D,    CMD_F,  SUPER_G,               KC_H,    CMD_J,    ALT_K,    CTL_L,  KC_SCLN,
         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,  KC_COMM,  LT3_DOT,  KC_SLSH,
-                                    KC_SPC,  LT2_TAB,            OSMLSFT,  LT1_ENT
+                                    LT1ENT,  LT2_TAB,            OSMLSFT,   LT1SPC
     ),
 
     /** Symbols and numbers
